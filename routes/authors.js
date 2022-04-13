017772514d80aba7dc5f9ae17bba0cf7d575b183
Madhusudhan <64250324+madhusudhan123-star@users.noreply.github.com>
2022-04-13 06:17:48 +0530

    add

---
+++ routes/authors.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const Author = require("../modules/author");
+
+router.get("/", (req, res) => {
+  res.render("authors/index");
+});
+
+router.get("/new", (req, res) => {
+  res.render("authors/new", { author: new Author() });
+});
+
+router.post("/", (req, res) => {
+  // res.send(req.body.name);
+  console.log(req.body);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== "production") {
-  require("dotenv");
+  require("dotenv").config();
 }
 
 const express = require("express");
@@ -7,6 +7,8 @@ const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
 const Routerindex = require("./routes/index");
+const authorsindex = require("./routes/authors");
+var bodyParser = require("body-parser");
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
@@ -18,4 +20,8 @@ app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
 app.use("/", Routerindex);
+app.use("/authors", authorsindex);
+// app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.listen(process.env.PORT || 5000);
